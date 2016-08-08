1934f1a3d88e0b4fa666ee0af9b60cf6f70a6c35
Patrick Todd <patrick.a.todd@gmail.com>
2016-08-08 13:04:55 -0400

    removed whitespace for easier readability

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
- res.render("index", {title: "Survey Form"});
+	res.render("index", {title: "Survey Form"});
 });
 
 app.post('/result', function(req, res) {
- console.log("results", req.body);
- var results = (req.body);
- console.log(results);
- res.render("results", {results: results});
+	console.log("results", req.body);
+ 	var results = (req.body);
+	console.log(results);
+	res.render("results", {results: results});
 });
 
 app.listen(8000, function() {
- console.log("listening on port 8000");
+	console.log("listening on port 8000");
 });
